e2f1767d1519eab48cda5dd768a24b8191a4a27a
Aman4102007 <amankumar3031.2@gmail.com>
2025-09-19 21:33:49 +0530

    Add files via upload

---
+++ Program to prove an integr even or odd by if-else statement.cpp
@@ -0,0 +1,14 @@
+// An integer is even or odd using if-else statement
+#include<stdio.h>
+int main(){
+	int n;
+	printf("Enter a number: ");
+	scanf("%d", &n);
+	if(n%2==0){
+		printf("The number is even");
+	}
+	else{
+		printf("The number is odd");
+	}
+	return 0;
+}
