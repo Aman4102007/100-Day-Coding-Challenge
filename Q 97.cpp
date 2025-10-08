8c14267ec43d5f764dc972ac4ff6110bd75f2f4c
Aman4102007 <amankumar3031.2@gmail.com>
2025-10-08 05:48:45 +0530

    Add files via upload

---
+++ Q 97.cpp
@@ -0,0 +1,23 @@
+// Program to initials 
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+int main(){
+	char s[50];
+	int i;
+	
+	printf("Enter a name\n");
+	gets(s);
+	
+	printf("The initials of the name is\n");
+	for(i = 0; i<strlen(s);i++){
+		if(i==0 && s[i]!=' '){
+			printf("%c", s[i]);
+		}
+		else if(s[i] == ' ' && s[i+1]!=' '){
+			printf(".%c", s[i+1]);
+		}
+	}
+	return 0;
+}
