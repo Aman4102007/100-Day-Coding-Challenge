a40bcdf0353a9799f1e66e0b26aefefb614ae98e
Aman4102007 <amankumar3031.2@gmail.com>
2025-10-09 05:53:12 +0530

    Add files via upload

---
+++ Q 100.cpp
@@ -0,0 +1,26 @@
+// Program to print all substrings of a string
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+	char str[100];
+	int i, j, k;
+	int len;
+	
+	printf("Enter a string: ");
+	scanf("%s", str);
+	
+	len = strlen(str);
+	
+	printf("All substrimgs of \%s\" are:\n",str);
+	
+	for (i = 0; i < len; i++){
+		for (j = i; j<len;j++){
+			for (k=i;k<=j;k++){
+				printf("%c", str[k]);
+			}
+			printf("\n");
+		}
+	}
+	return 0;
+}
